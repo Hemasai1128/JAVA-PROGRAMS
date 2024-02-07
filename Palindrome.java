7dbe7a5c7a05b52d118628007be3580f553f5fe3
Bukkapatnam Hemasai <119095226+Hemasai1128@users.noreply.github.com>
2024-02-07 19:40:52 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+//WAP to the given string and check if it is palindrome or not
+
+import java.lang.String;
+import java.util.Scanner;
+
+public class PalindromeCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value");
+        String str1 = sc.next();
+        
+        String str2 = "";
+        
+        for(int i=str1.length()-1;i>=0;i--)
+        {
+            str2 = str2 + str1.charAt(i);
+        }
+        if(str1.equals(str2))
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not a Palindrome");
+        }
+        
+    }
+}
